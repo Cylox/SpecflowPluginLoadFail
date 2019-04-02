7f50479669a49f451078f08b2b3ab75b274baaf5
Christian Pabst <christian_pabst@agilent.com>
2019-04-02 08:59:20 +0200

    Add project files.

---
+++ MyPlugin.SpecflowPlugin/MyGeneratorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+namespace MyPlugin.SpecflowPlugin
+{
+
+    public class MyGeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            Console.WriteLine("Dummy Plugin Initialized!");
+        }
+    }
+}
